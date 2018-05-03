b332496bfe7a49f4981d4f3919d3648df9e0b097
Sanchez <edgar.d.sanchez@accenture.com>
2018-05-03 16:39:56 -0500

    1st Project Euler problem

---
+++ peuler/p001.py
@@ -0,0 +1,10 @@
+# Project Euler Problem 1
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+sum = 0
+
+for i in range (1, 1001):
+	if i % 3 == 0 or i % 5 == 0:
+		sum += i
+
+print(sum)
